1cd97f4aa077712154645efc9f4b1daf1f1d5b4f
Sven Kreiss <me@svenkreiss.com>
2020-07-08 19:55:07 +0200

    update web build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-module.exports = {
+module.exports = [{
   mode: 'development',
   entry: './ts_src/replace.ts',
   devtool: 'inline-source-map',
@@ -20,4 +20,24 @@ module.exports = {
     filename: 'unicodeit.js',
     path: path.resolve(__dirname, 'ts_dist'),
   },
-};
+}, {
+  mode: 'development',
+  entry: './ts_src/ui.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'ui.js',
+    path: path.resolve(__dirname, 'web'),
+  },
+}];
